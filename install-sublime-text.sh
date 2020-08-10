cf1ac7c78fccbb7066a4a208482b78dc2dbd9800
Brandon Smith <vectorcell@gmail.com>
2020-08-09 22:32:24 -0500

    fixed last commit

--- install-sublime-text.sh
+++ install-sublime-text.sh
@@ -4,4 +4,5 @@ cd ~/Programs
 
 wget -q https://download.sublimetext.com/sublimehq-pub.gpg
 sudo apt-key add sublimehq-pub.gpg
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublimetext.list
 sudo apt update && sudo apt install sublime-text
