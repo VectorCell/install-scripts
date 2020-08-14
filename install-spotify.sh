6d08deb2d84aad61e933adda2d8d641170ea1daf
Brandon Smith <vectorcell@gmail.com>
2020-08-14 14:14:32 -0500

    minor fixes

--- install-spotify.sh
+++ install-spotify.sh
@@ -2,4 +2,4 @@
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 4773BD5E130D1D45
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update && sudo apt install spotify-client
+sudo apt update && sudo apt install -y spotify-client
--- install-sublime-text.sh
+++ install-sublime-text.sh
@@ -5,4 +5,4 @@ cd ~/Programs
 wget -q https://download.sublimetext.com/sublimehq-pub.gpg
 sudo apt-key add sublimehq-pub.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublimetext.list
-sudo apt update && sudo apt install sublime-text
+sudo apt update && sudo apt install -y sublime-text
