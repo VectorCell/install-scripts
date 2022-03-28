32ff39ad9881653c99cdd3356e94551829ec3888
Brandon Smith <vectorcell@gmail.com>
2022-03-28 08:23:31 -0500

    updated docker installation

--- install-docker-ce.sh
+++ install-docker-ce.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
-sudo apt update
-sudo apt install -y docker-ce
-sudo systemctl status docker.service
-
-sudo usermod -aG docker ${USER}
+# for Ubuntu 20.04 LTS
+sudo apt update && sudo apt install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-compose
+sudo adduser $USER docker
